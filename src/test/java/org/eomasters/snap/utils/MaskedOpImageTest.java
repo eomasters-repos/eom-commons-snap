e2799353677b13ec183d3575338ca01da24d3138
Marco <info@eomasters.org>
2024-07-30 12:27:37 +0200

    Remove unused imports from MaskedOpImageTest.java

--- src/test/java/org/eomasters/snap/utils/MaskedOpImageTest.java
+++ src/test/java/org/eomasters/snap/utils/MaskedOpImageTest.java
@@ -9,12 +9,12 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * -> http://www.gnu.org/licenses/gpl-3.0.html
@@ -28,8 +28,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.awt.image.Raster;
-import java.time.Duration;
-import java.time.LocalTime;
 import org.junit.jupiter.api.Test;
 
 public class MaskedOpImageTest {
